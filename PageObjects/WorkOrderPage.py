e413fc09752b1328fb09ac6cf4904df0e51b610c
admin <123456>
2018-04-20 16:59:11 +0800

    1

--- PageObjects/WorkOrderPage.py
+++ PageObjects/WorkOrderPage.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from selenium.webdriver.common.by import By
+from time import sleep
 
 
 class WorkOrderPage(object):
@@ -13,29 +14,52 @@ class WorkOrderPage(object):
     weixiu_buttton = (By.XPATH, "//input[@type='radio' and @data-type='3']")
     # 巡检button
     xunjian_buttton = (By.XPATH, "//input[@type='radio' and @data-type='4']")
+    """select1"""
     # 请选择派发流程select
     dispatch_flow_select = (By.XPATH, "//span[contains(text(), '请选择派发流程')]/following-sibling::div[1]/div/div/input[2]")
     # 请选择派发流程select下拉菜单
     dispatch_flow_selected_one = (By.XPATH, "//ul[@class='ivu-select-dropdown-list']/li[4]")
+    """select1"""
     # 项目名称input
     project_name_input = (By.XPATH, "//span[contains(text(), '项目名称')]/following-sibling::div[1]/div/input[1]")
     # 项目地点input
     project_place_input = (By.XPATH, "//span[contains(text(), '项目地点')]/following-sibling::div[1]/div/input[1]")
+    """select2"""
     # 负责人select下拉菜单
     principal_select = (By.XPATH, "//span[contains(text(), '负责人')]/following-sibling::div[1]/div/div/input[2]")
-    # 请选择负责人select下拉菜单
-    principal_selected_one = (By.XPATH, "//ul[@class='ivu-select-dropdown-list']/li[4]")
+    # 负责人下拉菜单列表-第一个选项
+    principal_selected_lists = (By.XPATH, "//ul[@class='ivu-select-dropdown-list']/li[1]")
+    """select2"""
     # 验证人select下拉菜单
     identifier_select = (By.XPATH, "//span[contains(text(), '验证人')]/following-sibling::div[1]/div/div/input[2]")
-    # 客户select下拉菜单
-    # customer_selects = (By.XPATH, "//li[contains(text(), '试用用户')]")
-    customer_selects = (By.XPATH, "//ul[@class='ivu-select-dropdown-list']/li[1]")
+    # 验证人select下拉菜单列表-第一个选项
+    identifier_selected_list = (By.XPATH, "//ul[@class='ivu-select-dropdown-list']/li[1]")
+    """select3"""
+    # 客户select下拉菜单-选择第一个用户
+    customer_select = (By.XPATH, "//span[contains(text(), '客户')]/following-sibling::div[1]/div/div/input[2]")
+    # 客户select下拉菜单列表-最后一个选项
+    customer_selected_lists = (By.XPATH, "//ul[@class='ivu-select-dropdown-list']/li[2]")
     # 客户联系人input
+    customer_linkman_input = (By.XPATH, "//span[contains(text(), '客户联系人')]/following-sibling::div[1]/div/input[1]")
     # 联系电话input
+    linkman_tel_input = (By.XPATH, "//span[contains(text(), '联系电话')]/following-sibling::div[1]/div/input[1]")
     # 工单内容描述input
-    # 计划开始时间select
-    # 计划结束时间select
+    work_order_content_description_input = (By.XPATH, "//span[contains(text(), '工单内容描述')]/following-sibling::div[1]/div/textarea")
+
+    # 计划开始时间input
+    plan_start_time_js = 'document.getElementsByTagName("input")[16].removeAttribute("readonly");'
+    plan_start_time_input = (By.XPATH, "//span[contains(text(), '计划开始时间')]/following-sibling::div[1]/div/div/div/input[1]")
+    # 计划时间确定button
+    plan_confirm_button1 = (By.XPATH, '//span[contains(text(), "确定")]')
+    plan_confirm_button2 = (By.XPATH, '//span[contains(text(), "确定")]')
+    # 计划结束时间input
+    plan_end_time_js = 'document.getElementsByTagName("input")[17].removeAttribute("readonly");'
+    plan_end_time_input = (By.XPATH, "//span[contains(text(), '计划结束时间')]/following-sibling::div[1]/div/div/div/input")
     # 班组输入input
+    group_input = (By.XPATH, "//span[contains(text(), '班组')]/following-sibling::div[1]/div/input[1]")
+    # 派单button
+    dispatch_button = (By.XPATH, "//button[contains(text(), '派单')]")
+
 
     # 我的待办button
     my_to_do_button = (By.XPATH, "//div[contains(text(), '我的待办')]")
@@ -124,5 +148,48 @@ class WorkOrderPage(object):
         driver.find_element(*self.principal_select).click()
 
     def click_principal_selected_one(self, driver):
-        el = driver.find_elements(*self.customer_selects)
+        el = driver.find_elements(*self.principal_selected_lists)
         el[1].click()
+
+    def click_identifier_select(self, driver):
+        driver.find_element(*self.identifier_select).click()
+
+    def click_identifier_selected_one(self, driver):
+        el = driver.find_elements(*self.identifier_selected_list)
+        el[2].click()
+
+    def click_customer_select(self, driver):
+        driver.find_element(*self.customer_select).click()
+
+    def click_customer_selected_one(self, driver):
+        el = driver.find_elements(*self.customer_selected_lists)
+        el[3].click()
+
+    def input_customer_linkman(self, driver, customer_linkman):
+        driver.find_element(*self.customer_linkman_input).send_keys(customer_linkman)
+
+    def input_linkman_tel(self, driver, linkman_tel):
+        driver.find_element(*self.linkman_tel_input).send_keys(linkman_tel)
+
+    def input_work_order_content_description(self, driver, work_order_content_description):
+        driver.find_element(*self.work_order_content_description_input).send_keys(work_order_content_description)
+
+    def select_plan_start_time(self, driver, plan_start_time):
+        driver.execute_script(self.plan_start_time_js)
+        sleep(1)
+        driver.find_element(*self.plan_start_time_input).send_keys(plan_start_time)
+        sleep(1)
+        driver.find_elements(*self.plan_confirm_button1)[0].click()
+
+    def select_plan_end_time(self, driver, plan_end_time):
+        driver.execute_script(self.plan_end_time_js)
+        sleep(1)
+        driver.find_element(*self.plan_end_time_input).send_keys(plan_end_time)
+        sleep(1)
+        driver.find_elements(*self.plan_confirm_button2)[1].click()
+
+    def input_group(self, driver, group):
+        driver.find_element(*self.group_input).send_keys(group)
+
+    def click_dispatch_button(self, driver):
+        driver.find_element(*self.dispatch_button).click()
--- test0004_smoke4.py
+++ test0004_smoke4.py
@@ -38,20 +38,72 @@ class Test(unittest.TestCase):
         workOrder1.click_add_order(driver=driver)
         """抢修类型"""
         workOrder1.click_qiangxiu_button(driver=driver)
-        """请选择派发流程"""
+        sleep(1)
+        driver.implicitly_wait(20)
+        """请选择派发流程_第2个抢修流程"""
         workOrder1.click_dispatch_flow_select(driver=driver)
-        sleep(3)
+        sleep(1)
+        driver.implicitly_wait(20)
+        sleep(1)
+        driver.implicitly_wait(20)
         """选择第一个派发"""
         workOrder1.click_dispatch_flow_selected_one(driver=driver)
+        sleep(1)
+        driver.implicitly_wait(20)
         """填写项目名称"""
         workOrder1.input_project_name(driver=driver, project_name=u'test项目名称')
+        sleep(1)
+        driver.implicitly_wait(20)
         """填写项目地点"""
         workOrder1.input_project_place(driver=driver, project_place=u'test项目地点')
+        sleep(1)
+        driver.implicitly_wait(20)
         """选择负责人"""
         workOrder1.click_principal_select(driver=driver)
-        """选择第一个负责人"""
+        sleep(1)
+        driver.implicitly_wait(20)
+        """选择第1个负责人"""
         workOrder1.click_principal_selected_one(driver=driver)
-
+        sleep(1)
+        driver.implicitly_wait(20)
+        """选择验证人"""
+        workOrder1.click_identifier_select(driver=driver)
+        sleep(1)
+        driver.implicitly_wait(20)
+        """选择第1个验证人"""
+        workOrder1.click_identifier_selected_one(driver=driver)
+        sleep(1)
+        driver.implicitly_wait(20)
+        """选择客户"""
+        workOrder1.click_customer_select(driver=driver)
+        sleep(1)
+        driver.implicitly_wait(20)
+        """选择第2个客户"""
+        workOrder1.click_customer_selected_one(driver=driver)
+        sleep(1)
+        driver.implicitly_wait(20)
+        """输入客户联系人"""
+        workOrder1.input_customer_linkman(driver=driver, customer_linkman=u'许臻')
+        sleep(1)
+        driver.implicitly_wait(20)
+        """输入联系电话"""
+        workOrder1.input_linkman_tel(driver=driver, linkman_tel='15609100803')
+        sleep(1)
+        driver.implicitly_wait(20)
+        """输入工单内容描述"""
+        workOrder1.input_work_order_content_description(driver=driver, work_order_content_description=u'工单内容描述')
+        sleep(1)
+        driver.implicitly_wait(20)
+        """输入计划开始时间"""
+        workOrder1.select_plan_start_time(driver=driver, plan_start_time='2018-04-20 00:00')
+        sleep(1)
+        driver.implicitly_wait(20)
+        """输入计划结束时间"""
+        workOrder1.select_plan_end_time(driver=driver, plan_end_time='2018-04-20 22:00')
+        """输入班组"""
+        workOrder1.input_group(driver=driver, group=u'巡检一班')
+        """点击派单按钮"""
+        workOrder1.click_dispatch_button(driver=driver)
 
         sleep(10)
         # 浏览器退出
